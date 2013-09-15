215a7dff8c6c02c84ffb594f3a7a499cdbeab97b
Luis Santana <lsantana.486@gmail.com>
2013-09-15 15:42:26 -0430

    Adding solution to homework 3

--- week5/homeworks.js
+++ week5/homeworks.js
@@ -1,3 +1,8 @@
 //Homework 5.2
 
 db.zip.aggregate([{$match:{"state":{$in:["CA","NY"]}}},{$group:{_id:{state:"$state",city:"$city"},pop:{$sum:"$pop"}}},{$match:{pop:{$gte:25000}}},{$group:{_id:1,avg_pop:{$avg:"$pop"}}}])
+
+//Homework 5.3
+
+db.grades.aggregate([{$unwind: "$scores"},{$match: {"scores.type":{"$in":["homework","exam"]}}},{$group: {_id:{"class":"$class_id","student_id":"$student_id"},"average":{$avg:"$scores.score"}}},{$group: {_id:{'class':"$_id.class"}, class_average:{$avg:"$average"}}},{$sort: {"class_average":1}}])
+
